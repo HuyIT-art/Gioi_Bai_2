45e3d4eefd27d475f1ee23632bff2cfd4b9e4720
HuyIT-art <quanghuybinhdan@gmail.com>
2026-01-12 11:22:48 +0700

    Add files via upload

---
+++ 04_create_schema_table_add_constraint.sql
@@ -0,0 +1,58 @@
+create schema elearning;
+
+create table elearning.Students(
+	student_id int generated always as identity primary key,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	email varchar(255) not null unique
+);
+
+create table elearning.Instructors(
+	instructor_id int generated always as identity primary key,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	email varchar(255) not null unique
+);
+
+create table elearning.Courses(
+	course_id int generated always as identity primary key,
+	course_name varchar(100) not null,
+	instructor_id int references elearning.Instructors(instructor_id)
+);
+
+create table elearning.Enrollments(
+	enrollment_id int generated always as identity primary key,
+	student_id int references elearning.Students(student_id),
+	course_id int references elearning.Courses(course_id),
+	enroll_date date not null
+);
+
+create table elearning.Assignments(
+	assignment_id int generated always as identity primary key,
+	title varchar(100) not null,
+	due_date date not null,
+	course_id int references elearning.Courses(course_id)
+);
+
+create table elearning.Submissions(
+	submission_id int generated always as identity primary key,
+	submission_date date not null,
+	grade float check(grade >= 0 and grade <=100),
+	assignment_id int references elearning.Assignments(assignment_id),
+	student_id int references elearning.Students(student_id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
